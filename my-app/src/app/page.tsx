2b48aa4c32ab85a2b48b1cf47cafc9b38de1ae58
Shishman <bardinclement97@gmail.com>
2024-02-05 12:17:10 +0100

    premier commit

---
+++ my-app/src/app/page.tsx
@@ -0,0 +1,5 @@
+export default function Home() {
+  return <div
+  className="h-0 w-0 border-b-[30px] border-l-[20px] border-r-[20px] border-b-black border-l-transparent border-r-transparent"
+/>
+}
